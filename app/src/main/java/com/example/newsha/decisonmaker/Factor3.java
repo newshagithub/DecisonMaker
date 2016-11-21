23e0f0d19943974486766ea1dd6f55c635864ad0
Newsha <newsha@umaryland.edu>
2016-11-21 15:35:19 -0500

    added activities for 5 factors

--- app/src/main/java/com/example/newsha/decisonmaker/Factor3.java
+++ app/src/main/java/com/example/newsha/decisonmaker/Factor3.java
@@ -3,11 +3,11 @@ package com.example.newsha.decisonmaker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Factor1 extends AppCompatActivity {
+public class Factor3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_factor1);
+        setContentView(R.layout.activity_factor3);
     }
 }
